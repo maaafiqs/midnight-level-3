7690ae407703d038644ff76914d886e8dfd2199f
maaafiqs <syafiqyusuf0308@gmail.com>
2026-09-11 07:58:44 +0800

    feat: add compact contract and managed types

---
+++ managed/counter.ts
@@ -0,0 +1,5 @@
+export const counter = {
+  read: (): number => 0,
+  write: (v: number): void => { void v; }
+};
+export const increment_by_private_amount = (secret: number): void => { void secret; };
